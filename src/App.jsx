80f56d27ff7d53d6c5262426b70d0d2373279716
yhavin <y.havin@gmail.com>
2023-01-25 15:27:49 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>What's up Grandma</h1>
+      <h2>It's time to make some food</h2>
+    </div>
+  );
+}
+
+export default App;

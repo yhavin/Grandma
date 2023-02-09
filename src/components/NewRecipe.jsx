ffbba45b4148f10ac2490a67387831e135c1a746
yhavin <y.havin@gmail.com>
2023-02-09 14:27:11 -0500

    Ingredients array field done

--- src/components/NewRecipe.jsx
+++ src/components/NewRecipe.jsx
@@ -11,7 +11,7 @@ const NewRecipe = () => {
     ingredients: [{ name: "" }]
   }
 
-  const { register, handleSubmit, control, reset, formState: { isSubmitSuccessful } } = useForm({
+  const { register, watch, handleSubmit, control, reset, formState: { submitCount, isSubmitSuccessful } } = useForm({
     defaultValues: defaultRecipe
   });
 
@@ -29,21 +29,39 @@ const NewRecipe = () => {
     move: ingredientMove,
   } = useFieldArray({ control, name: "ingredients" });
 
+  const watchIngredients = watch().ingredients;
+
+  const numIngredients = watchIngredients.filter(ingredient => ingredient.name !== "").length;
+
+  const handleMoveUp = index => {
+    index === 0 ? ingredientMove(index, watchIngredients.length - 1) : ingredientMove(index, index - 1)
+  }
+
+  const handleMoveDown = index => {
+    index === (watchIngredients.length - 1) ? ingredientMove(index, 0) : ingredientMove(index, index + 1)
+  }
+
+  const cleanSubmission = data => {
+    const cleanedIngredients = data.ingredients.filter(ingredient => ingredient.name !== "");
+    const cleanedData = {...data, ingredients: cleanedIngredients};
+    return cleanedData;
+  }
+
   const collectionId = collection(db, "recipes");
 
   const onSubmit = data => {
-    addDoc(collectionId, data);
+    data = cleanSubmission(data);
+    // addDoc(collectionId, data);
+    console.log(JSON.stringify(data, false, 2));
     reset(defaultRecipe);
   }
-
-  const numIngredients = ingredientFields.filter(obj => obj.name !== "").length;
 
   return (
     <div>
       <h2>New recipe</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label>Title</label>
-        <input {...register("title", { required: true })} placeholder="Title" />
+        <input {...register("title", { required: true })} type="search" placeholder="Title" />
         <label>Meal type</label>
         <select {...register("mealType")}>
           {mealTypes.map((mealType) => (
@@ -54,17 +72,17 @@ const NewRecipe = () => {
           <label>{numIngredients} Ingredient{numIngredients !== 1 && "s"}</label>
           {ingredientFields.map((ingredient, index) => (
             <li key={ingredient.id}>
-              <input {...register(`ingredients[${index}].name`)} placeholder="Ingredient" />
-              <button type="button" disabled={index === 0} onClick={() => ingredientMove(index, index - 1)}>&uarr;</button>
-              <button type="button" disabled={index === (numIngredients)} onClick={() => ingredientMove(index, index + 1)}>&darr;</button>
-              <button type="button" disabled={numIngredients === 0} onClick={() => ingredientRemove(index)}>Delete</button>
+              <input {...register(`ingredients[${index}].name`)} type="search" placeholder="Ingredient" />
+              <button type="button" disabled={watchIngredients.length === 1} onClick={() => handleMoveUp(index)}>&uarr;</button>
+              <button type="button" disabled={watchIngredients.length === 1} onClick={() => handleMoveDown(index)}>&darr;</button>
+              <button type="button" disabled={watchIngredients.length === 1} onClick={() => ingredientRemove(index)}>Delete</button>
             </li>
           ))}
           <button type="button" onClick={() => ingredientAppend({ name: "" })}>Add</button>
-          <button type="button" disabled={numIngredients === 0} onClick={() => { ingredientRemove(); ingredientAppend({ name: "" }) }}>Clear</button>
+          <button type="button" disabled={watchIngredients.length === 1} onClick={() => { ingredientRemove(); ingredientAppend({ name: "" }) }}>Clear</button>
         </ul>
         <input type="submit" />
-        <p>{"isSubmitSuccessful: " + isSubmitSuccessful}</p>
+        <p>{submitCount > 0 && "isSubmitSuccessful: " + isSubmitSuccessful}</p>
       </form>
     </div>
   );

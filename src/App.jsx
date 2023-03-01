933c4a64f504472c01bb4d154f8ef8918b394238
yhavin <y.havin@gmail.com>
2023-03-01 02:18:42 -0500

    Small update

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ export const theme = createTheme({
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/public">
+      <BrowserRouter basename="/">
         <Routes>
           <Route exact path= "/" element={<Login />} />
           <Route exact path="/recipes" element={<Recipes />} />
